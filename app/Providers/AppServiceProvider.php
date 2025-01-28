c53279634b0733ee276162acfc2dd3063a7ff324
Alex Chamara <cb011374@students.apiit.lk>
2025-01-28 10:14:35 +0530

    http again 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Category;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
         View::composer('*', function ($view) {
             $view->with('categories', Category::all(['id', 'name']));
         });
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     
     }
 }
